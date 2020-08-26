17dc6f2ab36e2fde6a679e8a0c62054a85e27ad6
Allenlee521 <70263724+Allenlee521@users.noreply.github.com>
2020-08-26 17:40:31 +0800

    Create 8001.js

---
+++ univ/8001.js
@@ -0,0 +1,23 @@
+var univ_list = {
+    school: "南京大学",
+    id: "8001",
+    link: [ {
+        name: "南大主页",
+        url: "https://www.nju.edu.cn/"
+    }, {
+        name: "教务系统",
+        url: "http://elite.nju.edu.cn/jiaowu/exit.do/"
+    }, {
+        name: "图书馆",
+        url: "http://lib.nju.edu.cn/index.htm/"
+    }, {
+        name: "小百合",
+        url: "http://bbs.nju.edu.cn/"
+    }, {
+        name: "办事大厅",
+        url: "http://ehall.nju.edu.cn/"
+    }, {
+        name: "紫荆站",
+        url: "http://zijingbt.njuftp.org/index.html"
+    } ]
+};
